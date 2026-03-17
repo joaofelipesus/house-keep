0ff1dd5e3844ac4adb28ddb3122a948e0ca0801f
joaofelipe <joaofelipe1294@gmail.com>
2026-03-17 19:58:29 -0300

    fix: required fields

--- db/migrate/20260317223243_create_bills.rb
+++ db/migrate/20260317223243_create_bills.rb
@@ -5,7 +5,7 @@ class CreateBills < ActiveRecord::Migration[8.1]
 
       t.boolean :recurring, comment: 'Define if the bill is paid every month'
       t.decimal :value, precision: 10, scale: 2, null: false, comment: 'bill value'
-      t.string :title, comment: 'a simple description'
+      t.string :title, null: false, comment: 'a simple description'
       t.date :due_date, null: false, comment: 'when the bill is charged'
       t.text :description, comment: 'an optional text with a detailed description'
       t.string :payment_method, comment: 'An enum with the payment method, e.g. credit card, or pix'
