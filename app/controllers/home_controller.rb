9650fa79d4ec60d3a3f667caf02ff59807ad97b6
joaofelipe <joaofelipe1294@gmail.com>
2026-05-13 17:16:55 -0300

    feat: render the list of invoices

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @pending_invoices = Invoice.pending
+                               .where(due_date: Date.current.beginning_of_month..Date.current.end_of_month)
+                               .includes(:bill)
+                               .order(:due_date)
+  end
 end
