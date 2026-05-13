512785dfef31bc588ec6e5812bbfdab40d12cd18
joaofelipe <joaofelipe1294@gmail.com>
2026-05-13 11:05:29 -0300

    fix: set payment_status default value

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,5 @@
 class Invoice < ApplicationRecord
   belongs_to :bill
+
+  enum :payment_status, { pending: "pending", paid: "paid", delayed: "delayed" }, default: "pending"
 end
--- db/migrate/20260513135655_create_invoices.rb
+++ db/migrate/20260513135655_create_invoices.rb
@@ -4,7 +4,7 @@ class CreateInvoices < ActiveRecord::Migration[8.1]
       t.timestamps
 
       t.belongs_to :bill, null: false, foreign_key: true
-      t.string :payment_status, null: false, comment: 'A enum with the payment status of the invoice (pending, paid, delayed)'
+      t.string :payment_status, null: false, default: 'pending', comment: 'A enum with the payment status of the invoice (pending, paid, delayed)'
       t.date :payment_date, comment: 'The date when the bill was paid'
       t.text :comment, comment: 'A optional description of the invoice'
     end
