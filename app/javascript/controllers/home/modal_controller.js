03c0afb0a36b96fb06e7d9642feb6d419c21e4bb
joaofelipe <joaofelipe1294@gmail.com>
2026-05-14 08:27:43 -0300

    feat: add home modal

---
+++ app/javascript/controllers/home/modal_controller.js
@@ -0,0 +1,37 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="home--modal"
+export default class extends Controller {
+  static targets = ['overlay', 'frame']
+
+  connect() {
+    this.element.addEventListener('turbo:submit-end', this.#handleSubmitEnd)
+  }
+
+  disconnect() {
+    this.element.removeEventListener('turbo:submit-end', this.#handleSubmitEnd)
+  }
+
+  open(event) {
+    event.preventDefault()
+    this.frameTarget.src = event.params.url
+    this.overlayTarget.classList.add('is-open')
+  }
+
+  close() {
+    this.overlayTarget.classList.remove('is-open')
+    this.frameTarget.removeAttribute('src')
+    this.frameTarget.innerHTML = ''
+  }
+
+  closeOnBackdrop(event) {
+    if (event.target === this.overlayTarget) this.close()
+  }
+
+  #handleSubmitEnd = ({ detail: { success } }) => {
+    if (success) {
+      this.close()
+      Turbo.visit(window.location.href)
+    }
+  }
+}
