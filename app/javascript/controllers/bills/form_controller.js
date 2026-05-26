a758cf1e04010ba785714925a312276b3394d150
joaofelipe <joaofelipe1294@gmail.com>
2026-05-25 22:36:44 -0300

    feat: add image logo

--- app/javascript/controllers/bills/form_controller.js
+++ app/javascript/controllers/bills/form_controller.js
@@ -5,15 +5,29 @@ export default class extends Controller {
   static targets = [
     'recurringInput',
     'nonRecurringSection',
-    'recurringSection'
+    'recurringSection',
+    'logoPreview',
+    'logoHint'
   ]
 
   connect() {
     this.#applyVisibility(this.recurringInputTarget.checked)
+    this.logoPreviewTarget.hidden = !this.logoPreviewTarget.getAttribute('src')
   }
 
   toggleRecurring(event) {
     this.#applyVisibility(event.target.checked)
+  }
+
+  previewLogo(event) {
+    const file = event.target.files[0]
+    if (!file) return
+
+    const url = URL.createObjectURL(file)
+    this.logoPreviewTarget.src = url
+    this.logoPreviewTarget.hidden = false
+    this.logoHintTarget.textContent = file.name
+    this.logoHintTarget.classList.add('file-upload-hint--filled')
   }
 
   #applyVisibility(isRecurring) {
