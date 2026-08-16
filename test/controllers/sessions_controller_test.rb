add2935320c87e69c98c2b0cf1111896ae313a73
joaofelipe <joaofelipe1294@gmail.com>
2026-08-16 13:30:41 -0300

    chore: fix authentication

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -8,6 +8,29 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "signed out navbar has no links, no income and no sign out button" do
+    get new_session_path
+
+    assert_select 'nav.navbar'
+    assert_select 'nav.navbar a', count: 0
+    assert_select '.navbar-links', count: 0
+    assert_select '.navbar-income', count: 0
+    assert_select '.navbar-signout', count: 0
+  end
+
+  test "signed in navbar has the links, the income and a sign out button" do
+    sign_in_as @user
+
+    get root_path
+
+    assert_select '.navbar-links a', count: 2
+    assert_select '.navbar-income-value'
+    assert_select ".navbar-signout[action=?]", session_path do
+      assert_select "input[name=_method][value=delete]", count: 1
+      assert_select "button", text: 'Sair'
+    end
+  end
+
   test "create with valid credentials" do
     post session_path, params: { email_address: @user.email_address, password: "password" }
 
@@ -20,6 +43,7 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to new_session_path
     assert_nil cookies[:session_id]
+    assert_equal 'E-mail ou senha inválidos.', flash[:alert]
   end
 
   test "destroy" do
